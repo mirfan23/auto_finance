68b67a6f92cc35f5099f8aba7905c5293a21515c
irfan fadhila <irfanfadhihla000@gmail.com>
2026-06-22 08:10:37 +0700

    initial commit: architecture

---
+++ android/app/src/main/kotlin/com/example/auto_finance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.auto_finance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/router.dart
@@ -0,0 +1,11 @@
+// import 'package:auto_finance/features/dashboard/pages/dashboard_page.dart';
+// import 'package:auto_finance/features/notification_listener/pages/notification_page.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    // GoRoute(path: '/', builder: (_, __) => const DashboardPage()),
+    // GoRoute(path: '/notification', builder: (_, __) => const NotificationPage()),
+  ],
+);
